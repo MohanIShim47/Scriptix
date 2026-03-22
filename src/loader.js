0a8cc24d92a6a8b141088ca858c6acf16115dd66
TEDA <mohanasudan63@gmail.com>
2026-03-22 12:05:20 -0400

    fixed loader

--- src/loader.js
+++ src/loader.js
@@ -1,3 +1,3 @@
 var script = document.createElement("script");
-script.src = "https://raw-githack-com.translate.goog/MohanIShim47/Scriptix/main/src/main.js";
+script.src = "hhttps://raw-githack-com.translate.goog/MohanIShim47/Scriptix/main/src/main.js";
 document.head.appendChild(script);
